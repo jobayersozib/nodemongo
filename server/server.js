95695b49c220dac8ce3e1ce61e334f6b1405477f
Faysal Ahamed <faysal@webpers.com>
2018-02-01 15:09:15 +0600

    changed

--- server/server.js
+++ server/server.js
@@ -2,23 +2,47 @@
 var mongoose=require("../models/db");
 var todo=require("../models/todo");
 var user=require("../models/user");
-var app=require("express")
-app.listen(3000,function(){
+var express=require("express");
+var bodyParser=require("body-parser")
+var app=express();
+app.use(bodyParser.json())
+var obj={
+    text:"Another todo task",
+    completed:false,
+    completedAt:123
+}
+
+app.get("/todos",(req,res)=>{
+todo.Todo.find({completed:false}).
+  limit(10).
+  sort({ occupation: -1 }).
+  select({ text: 1,_id:0}).
+  exec(function(error,result){
+    res.send(result);
+  });
+    
+});
+
+
+app.post("/todos",(req,res)=>{
+    obj.text=req.body.Text;
+    obj.completed=req.body.completed;
+    obj.completedAt=req.body.time;
+    var newTodo=new todo.Todo(obj);
+    newTodo.save().then((doc)=>{
+        res.send(doc+" Added to database")
+    },(e)=>{
+        res.send("Unable to add  to database")
+    });
+    console.log(req.body);
+})
+
+app.listen(process.env.PORT,function(){
     console.log("Started at port:30000")
 })
 
 
-// var obj={
-//     text:"Another todo task",
-//     completed:false,
-//     completedAt:123
-// }
-// var newTodo=new Todo(obj);
-// newTodo.save().then((doc)=>{
-//     console.log("Saved todo"+doc)
-// },(e)=>{
-//     console.log("Unable to save")
-// });
+
 
 
 // var addUser=new user.User({Email:"Jobayer@webpers.com"});
